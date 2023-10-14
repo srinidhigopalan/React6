aecdabeb478b13a6e3f100588e5ca29badcafa63
srinidhigopalan <srinidhigo@gmail.com>
2023-10-15 00:44:48 +0530

    first

---
+++ src/data/constants.js
@@ -0,0 +1,169 @@
+export const Bio = {
+    name: "Srinidhi Gopalan",
+    roles: [
+      "Mern Stack",
+      "Data Science",
+      "Machine Learning",
+      "Business Analytics"
+    ],
+    description:
+      "A computer science undergraduate at Vellore Institute of Technology specializing in artificial intelligence and robotics who takes keen interest in working on projects related to Machine Learning and Data Science. I enjoy working on public projects where I can collaborate with people and broaden my skills. Looking forward for an opportunity where I can put my knowledge into practice under the most challenging circumstances.",
+    github: "https://github.com/srinidhigopalan",
+    resume:
+      "https://drive.google.com/file/d/1evwKysnYa81qeGqLEooo48N3rSr3VEhx/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/srinidhi-gopalan/",
+  };
+  
+  export const skills = [
+    {
+      title: "Mern",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+          "https://w1.pngwing.com/pngs/711/379/png-transparent-green-grass-mongodb-database-documentoriented-database-dashboard-nosql-bson-javascript.png",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Node Js",
+          image:
+         "https://p1.hiclipart.com/preview/885/534/395/green-grass-nodejs-javascript-react-mean-angularjs-logo-symbol-png-clipart.jpg",
+        },
+      ],
+    },
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image: "https://codersera.com/blog/wp-content/uploads/2019/06/java_logo_640.jpg",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/926px-C_Programming_Language.svg.png",
+        },
+        {
+          name: "C++",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://1000logos.net/wp-content/uploads/2020/08/MySQL-Logo.jpg",
+        },
+        {
+          name: "R",
+          image: "https://e7.pngegg.com/pngimages/801/880/png-clipart-rstudio-macos-r-blue-text.png",
+        },
+        {
+          name: "MATLAB",
+          image:
+            "https://banner2.cleanpng.com/20180703/yrf/kisspng-matlab-simulink-signal-processing-programming-lang-cube-island-online-survival-3d-5b3b394d2f1946.2068935715306079491929.jpg",
+        },
+
+      ],
+    },
+    
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C560BAQHEq7e9Jg7BTg/company-logo_200_200/0/1590208225946?e=2147483647&v=beta&t=eSwTqymeM9SEKbMP1Mj8zUOiSV8y95KjZIa9lj6ByzE",
+      role: "Data Science Intern",
+      company: "Ioncure",
+      date: "October 2023 - Present",
+      desc: "I work as a Data Science Intern at Ioncure.",
+      skills: [
+        "Data Science",
+        "Data Analytics",
+        "Research",
+        "Webscrapping",
+        "Machine Learning",
+      ],
+    },
+    {
+      id: 0,
+      img: "https://img.etimg.com/thumb/msid-103375522,width-1200,height-900,resizemode-4,imgsize-7650/tech-mahindra-stocks-live-updates-05-sep-2023.jpg",
+      role: "Software Engineer Intern",
+      company: "Tech Mahindra",
+      date: "October 2023 - Present",
+      desc: "I work as a Software Engineering Intern at Tech Mahindra",
+      skills: [
+        "Programming",
+        "Database Management",
+        "Operating Systems",
+        "Computer Networks",
+        "Debugging",
+        "Testing",
+        
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://findlogovector.com/wp-content/uploads/2022/05/vellore-institute-of-technology-vit-logo-vector-2022.png",
+      school: "Vellore Institute of Technology",
+      date: "2021-2025",
+     //grade: "8.71 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering with spl. in AI and Robotics at Vellore Institute of Technology.",
+      degree: "B.Tech",
+    },
+    {
+      id: 1,
+      img: "https://www.schoolmegamart.com/wp-content/uploads/2021/04/sfds-1.png",
+      school: "St. Francis de Sales Senior Secondary School, New Delhi",
+      date: "2021",
+      grade: "95.6%",
+      desc: "I completed my 12th grade education at St. Francis de Sales Senior Secondary School, where I studied Physics, Chemistry and Mathematics with Computer Science.",
+      degree: "Central Board of Secondary Education (CBSE) XII",
+    },
+    {
+      id: 2,
+      img: "https://www.schoolmegamart.com/wp-content/uploads/2021/04/sfds-1.png",
+      school: "St. Francis de Sales Senior Secondary School, New Delhi",
+      date: "2019",
+      grade: "91.4%",
+      desc: "I completed my 10th grade education at St. Francis de Sales Senior Secondary School.",
+      degree: "Central Board of Secondary Education (CBSE) X",
+    },
+  ];
+  
+ 
